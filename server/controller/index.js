1fc1809c12bf8249ffa401191662f3e7fda7c222
Roinand Aguila <rbaguila1@up.edu.ph>
2017-06-30 04:05:40 +0000

    Finished CRUD operations for carrier table

--- server/controller/index.js
+++ server/controller/index.js
@@ -20,10 +20,24 @@ exports.viewCarriers = (req, res, next) => {
   });
 };
 
+//View individual carriers
+exports.viewCarrier = (req, res, next) => {
+  const id = req.params.id;
+  pool.query("SELECT ID, RTRIM(Name) AS Name, \
+  RTRIM(Description) AS Description \
+  FROM carriers WHERE ID = "+id, 
+  (err, result) => {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    
+    res.status(200).send(result.rows);
+  
+  });
+};
+
 //Add a carrier
 exports.addCarrier = (req, res, next) => {
-  console.log("Received request");
-  
   //used destructuring assignment for variables that will be used for the insert query
   let name, description;
   ({name, description} = req.body);
@@ -39,48 +53,32 @@ exports.addCarrier = (req, res, next) => {
   
 };
 
-// //Edit a user via his userID
-// exports.editUser = (req, res) =>{
-//     User.findById(req.params.user_id, function(err, user) {
-//         if (err){
-//             res.send(err);
-//         }else{
-//             //setting the new username and password to whatever was changed. If 
-//             //nothing was changed we will not alter the field.
-//             (req.body.username) ? user.username = req.body.username : null;
-//             (req.body.password) ? user.password = req.body.password : null;
-//             (req.body.firstName) ? user.firstName = req.body.firstName : null;
-//             (req.body.middleName) ? user.middleName = req.body.middleName : null;
-//             (req.body.lastName) ? user.lastName = req.body.lastName : null;
+//Update a carrier
+exports.editCarrier = (req, res, next) => {
+  const id = req.params.id;
+  
+  let name, description;
+  ({name, description} = req.body);
+  
+  pool.query("UPDATE carriers SET Name = '"+name+"', Description = '"+description+"' \
+    WHERE ID = "+id , 
+  (err, result) => {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    res.json({ message: 'Carrier successfully updated!' });
+  });
+};
 
-//             (req.body.officeAddress) ? user.officeAddress = req.body.officeAddress : null;
-//             (req.body.homeAddress) ? user.homeAddress = req.body.homeAddress : null;
-//             (req.body.civilStatus) ? user.civilStatus = req.body.civilStatus : null;
-//             (req.body.occupation) ? user.occupation = req.body.occupation : null;
-//             (req.body.age) ? user.age = req.body.age : null;
-//             (req.body.sex) ? user.sex = req.body.sex : null;
-
-//             (req.body.birthDate) ? user.birthDate = req.body.birthDate : null;
-//             (req.body.refferedBy) ? user.refferedBy = req.body.refferedBy : null;
-//             (req.body.contactNumber) ? user.contactNumber = req.body.contactNumber : null;
-//         }
-        
-//         //save user
-//         user.save(function(err) {
-//             if (err)
-//             res.send(err);
-//             res.json({ message: 'User has been updated' });
-//         });
-//     });
-// };
-
-// //Deletes a user
-// exports.deleteUser = (req, res) =>{
-//     User.remove({ _id: req.params.user_id }, function(err, user) {
-//         if (err){
-//             res.send(err);
-//         }else{
-//              res.json({ message: 'User has been deleted' })
-//         }
-//     });
-// }
+//Delete a carrier
+exports.deleteCarrier = (req, res, next) => {
+  const id = req.params.id;
+  
+  pool.query("DELETE FROM carriers WHERE ID = "+id , 
+  (err, result) => {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    res.json({ message: 'Carrier successfully deleted!' });
+  });
+};
--- server/router.js
+++ server/router.js
@@ -1,17 +1,14 @@
-const controller = require(__dirname + '/controller')
+const controller = require(__dirname + '/controller');
 
 module.exports = (router) => {
 
     //Carriers
     router.get('/', controller.initializeAPI);
     router.get('/carriers', controller.viewCarriers);
+    router.get('/carrier/:id', controller.viewCarrier);
     router.post('/carriers', controller.addCarrier);
-
-    // //Edit functions
-    // router.put('/user/:user_id', user.editUser);
-
-    // //Delete functions
-    // router.delete('/user/:user_id', user.deleteUser);
+    router.put('/carrier/:id', controller.editCarrier);
+    router.delete('/carrier/:id', controller.deleteCarrier);
 
     return router;
 }
